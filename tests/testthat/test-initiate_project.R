7e4977522d0a5047b01bd869a7b69c712549b414
Stefan Vermeent <p.c.s.vermeent@gmail.com>
2023-05-18 11:03:50 +0200

    unit tests for initiate_project()

--- tests/testthat/test-initiate_project.R
+++ tests/testthat/test-initiate_project.R
@@ -1,3 +1,67 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("creating a single study folder structure works", {
+
+  current_wd <- getwd()
+  path = file.path(tempdir(), "temp-repo") |> gsub(x = _, pattern = "\\\\", replacement = "/")
+
+  on.exit(unlink(path, recursive = TRUE), add = TRUE)
+
+  testthat::local_mocked_bindings(
+    use_git = function(...) TRUE, .package = 'usethis'
+  )
+  testthat::local_mocked_bindings(
+    use_github = function(...) "This one connects to github", .package = 'usethis'
+  )
+  testthat::local_mocked_bindings(
+    get_git_url = function(...) "https://github.com/username/repo-name"
+  )
+
+
+  initiate_project(path = path, project = "single_study")
+
+  expect_true(file.exists(path))
+  expect_true(file.exists(file.path(path, "README.Rmd")))
+  expect_true(file.exists(file.path(path, ".projectlog")))
+  expect_true(file.exists(file.path(path, ".projectlog", "MD5")))
+  expect_true(file.exists(file.path(path, "manuscript", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "supplement", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "materials", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "preregistration", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "data")))
+  expect_true(file.exists(file.path(path, "scripts")))
+
+  setwd(current_wd)
+})
+
+
+test_that("creating a multistudy folder structure works", {
+
+  current_wd <- getwd()
+  path = file.path(tempdir(), "temp-repo")
+
+  on.exit(unlink(path, recursive = TRUE), add = TRUE)
+
+  testthat::local_mocked_bindings(
+    use_git = function(...) TRUE, .package = 'usethis'
+  )
+  testthat::local_mocked_bindings(
+    use_github = function(...) "This one connects to github", .package = 'usethis'
+  )
+  testthat::local_mocked_bindings(
+    get_git_url = function(...) "https://github.com/username/repo-name"
+  )
+
+  initiate_project(path = path, project = "multistudy")
+
+  expect_true(file.exists(path))
+  expect_true(file.exists(file.path(path, ".projectlog")))
+  expect_true(file.exists(file.path(path, ".projectlog", "MD5")))
+  expect_true(file.exists(file.path(path, "manuscript", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "supplement", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "study1", "materials", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "study1", "preregistration", "README.Rmd")))
+  expect_true(file.exists(file.path(path, "study1", "data")))
+  expect_true(file.exists(file.path(path, "study1", "scripts")))
+
+  setwd(current_wd)
 })
